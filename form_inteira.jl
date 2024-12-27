fdff1e55fa2064e47c509c9f8f945c9e1f786c94
Maiara Trevisol <maiara.trevisol@sap.com>
2024-12-27 20:54:34 -0300

    adiciona logs para form inteira

--- form_inteira.jl
+++ form_inteira.jl
@@ -2,10 +2,12 @@ using Pkg
 Pkg.add("JuMP")
 Pkg.add("HiGHS")
 Pkg.add("Random")
+Pkg.add("Dates")
 
 using JuMP
 using HiGHS
 using Random
+using Dates
 
 function read_file(file_path)
     open(file_path, "r") do file
@@ -13,13 +15,13 @@ function read_file(file_path)
         M = parse(Int, readline(file))
         T = parse(Int, readline(file))
         m = parse(Int, readline(file))
-        
+
         attractions = []
         for _ in 1:m
             theme, dimension = split(readline(file)) .|> x -> parse(Int, string(x))
             push!(attractions, (theme, dimension))
         end
-        
+
         return n, M, T, m, attractions
     end
 end
@@ -47,9 +49,9 @@ function create_model(n, M, T, m, attractions)
             @constraint(model, x[i, j] <= y[i, theme])
         end
     end
-    
+
     @objective(model, Min, sum(y[i, t] for i in 1:n, t in 1:T))
-    
+
     return model, x, y
 end
 
@@ -66,23 +68,36 @@ function main(args)
 
     Random.seed!(seed)
 
+  
+   
+
     n, M, T, m, attractions = read_file(file_path)
 
     model, x, y = create_model(n, M, T, m, attractions)
 
+    initial_time = now()
+
     set_time_limit_sec(model, timeout)
-    
     set_attribute(model, "random_seed", seed)
     
     optimize!(model)
+
+    final_time = now()
+    tempo_execucao = (Dates.value(final_time) - Dates.value(initial_time)) / 1000.0
 
     status = JuMP.termination_status(model)
     
     if status == MOI.OPTIMAL || (status == MOI.TIME_LIMIT && JuMP.primal_status(model) == MOI.FEASIBLE_POINT)
         valor_objetivo = objective_value(model)
-        solution_x = JuMP.value.(x)  
-        
+        solution_x = JuMP.value.(x)
+        println("Nome da instância: $file_path")
         println("Melhor solucão encontrada: $valor_objetivo")
+        if status == MOI.TIME_LIMIT
+            upper_bound = JuMP.objective_bound(model)
+            println("Limite superior alcançado: $upper_bound")
+        end
+        println("Tempo médio de execução da formulação (segundos): $tempo_execucao")
+        println("Valor do parâmetro escolhido ou da semente de aleatoriedade: $seed")
         println("detalhes da solução:")
         for i in 1:n
             atracoes_atribuidas = [j for j in 1:m if solution_x[i, j] > 0.5]
